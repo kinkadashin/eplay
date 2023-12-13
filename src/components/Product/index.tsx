8b0ac4cc7eb0b987f20fe490ac14c0a541579886
Lucas Tavares <lucastavaresffr@gmail.com>
2023-12-13 14:57:51 -0300

    usando array de strings para estilizar diferentes tags

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,18 +1,35 @@
 import Tag from '../Tag'
 
-import { Card, Descricao, Titulo } from './styles'
+import { Card, Descricao, Titulo, Infos } from './styles'
 
-const Product = () => (
+type Props = {
+  title: string
+  category: string
+  system: string
+  description: string
+  infos: string[]
+  image: string
+}
+
+const Product = ({
+  title,
+  category,
+  system,
+  description,
+  infos,
+  image
+}: Props) => (
   <Card>
-    <img src="//placehold.it/222x250" alt="" />
-    <Titulo>Nome do jogo</Titulo>
-    <Tag>Categoria</Tag>
-    <Tag>Windows</Tag>
-    <Descricao>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa ipsum vero,
-      aliquam a enim assumenda unde molestiae sit id! Beatae blanditiis
-      quibusdam a maxime maiores officia tempora repudiandae sunt repellat.
-    </Descricao>
+    <img src={image} alt={title} />
+    <Infos>
+      {infos.map((info) => (
+        <Tag key={info}>{info}</Tag>
+      ))}
+    </Infos>
+    <Titulo>{title}</Titulo>
+    <Tag>{category}</Tag>
+    <Tag>{system}</Tag>
+    <Descricao>{description}</Descricao>
   </Card>
 )
 
--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -2,20 +2,48 @@ import Product from '../Product'
 
 import { Container, List } from './styles'
 
-type Props = {
+export type Props = {
   title: string
   background: 'gray' | 'black'
 }
 
 const ProductsList = ({ background, title }: Props) => (
-  <Container>
+  <Container background={background}>
     <div className="container">
       <h2>{title}</h2>
       <List>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        <Product
+          category="Ação"
+          description="Teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Jogos Vorazes"
+        />
+        <Product
+          category="Ação"
+          description="Teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Jogos Vorazes"
+        />
+        <Product
+          category="Ação"
+          description="Teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Jogos Vorazes"
+        />
+        <Product
+          category="Ação"
+          description="Teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Jogos Vorazes"
+        />
       </List>
     </div>
   </Container>
